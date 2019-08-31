e0ddcbbb78c6d567fac8548ee1c2f2d09e1712f9
Ilko <ilkom2@medtronic.com>
2019-08-31 19:44:06 +0200

    JS if ! iPhone...

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 // SHOW AND HIDE NAVBAR ON SCROLL
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbarid").style.top = "0";
-  } else {
-    document.getElementById("navbarid").style.top = "-60px";
+if (!(/iPhone|iPad|iPod|Android|webOS|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent))) {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbarid").style.top = "0";
+    } else {
+      document.getElementById("navbarid").style.top = "-60px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
 }
 
 
 // HIDE NAVBAR LI ON CLICK
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$('.navbar-nav>li>a').on('click', function() {
+  $('.navbar-collapse').collapse('hide');
 });
